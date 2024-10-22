53a5ad903856b01a17ab35d6a7e3f57a6028180e
Janan Panchal <jpanchal@ualberta.ca>
2024-10-22 12:51:29 -0600

    Test should fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,41 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> cities;
+    private CustomList list;
+
+    /**
+     * Get the number of cities in the list
+     * @return Number of cities in the list
+     */
+    public int getCount() {
+        return cities.size();
+    }
+
+    /**
+     * Add a city to the list
+     * @param city City to add
+     */
+    public void addCity(City city) {
+
+    }
+
+    /**
+     * Create a mocklist
+     * @return Mocklist
+     */
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest() {
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 
 
 }
