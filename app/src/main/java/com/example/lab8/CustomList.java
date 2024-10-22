ffe09901e1f51f9ca8f41e57268be9791efdba1a
Janan Panchal <jpanchal@ualberta.ca>
2024-10-22 13:32:50 -0600

    Implement countCities - should pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -75,7 +75,7 @@ public class CustomList extends ArrayAdapter<City> {
      * @return The number of cities in the list
      */
     public int countCities() {
-        /*return cities.size();*/
+        return cities.size();
     }
 
 }
